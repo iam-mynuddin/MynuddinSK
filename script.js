e9b82584813ecafa0e0fbfc4f95738e3f2c258df
iam-mynuddin <mynuddin.shaik234@gmail.com>
2024-05-25 16:54:02 +0530

    May 25th

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            
+            const targetId = this.getAttribute('href').substr(1);
+            const targetElement = document.getElementById(targetId);
+            
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+
 function openGoogleForm(){
     window.open('https://forms.gle/MKCop3yjCXzD4jMy8','_blank')
 }
