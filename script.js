4529178b85fa211d6bad89dedc3c02c6a11b8523
iam-mynuddin <mynuddin.shaik234@gmail.com>
2024-05-25 17:03:40 +0530

    a

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+    // URL of the file you want to download from GitHub Pages
+    const fileUrl = 'https://yourusername.github.io/yourrepository/path/to/your/example.txt';
+
+    // Create a temporary anchor element
+    const a = document.createElement('a');
+    a.href = fileUrl;
+    a.download = 'Resume_Mynuddin';  // Name of the file to be downloaded
+
+    // Append the anchor element to the body (required for Firefox)
+    document.body.appendChild(a);
+
+    // Programmatically click the anchor element
+    a.click();
+
+    // Remove the anchor element from the document
+    document.body.removeChild(a);
+};
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
